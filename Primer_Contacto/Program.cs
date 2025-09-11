d95ed9d4609f26ebbadec0d2c3f0c54ef2736a2f
Alejandro del Valle Vallés <alejandrodelvalle06@outlook.es>
2025-09-11 19:33:32 +0200

    Ejercicio array bidimensional

---
+++ Primer_Contacto/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Primer_Contacto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = {
+                {1, 2, 3},
+                {4, 5, 6}
+            };
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
