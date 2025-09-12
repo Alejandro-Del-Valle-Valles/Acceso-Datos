2c502ae28a84669717a3627f1b73e5a6274a6dd5
Alejandro Del Valle <alejandrodelvalle06@outlook.es>
2025-09-12 12:28:45 +0200

    Creo el tema 01

--- Primer_Contacto/Program.cs
+++ Primer_Contacto/Program.cs
@@ -9,6 +9,12 @@
                 {4, 5, 6}
             };
 
+            byte[][] numbersByte = new byte[2][]
+            {
+                new byte[] { 1, 2, 3 },
+                new byte[] { 4, 5, 6 }
+            };
+
             foreach (int number in numbers)
             {
                 Console.WriteLine(number);
