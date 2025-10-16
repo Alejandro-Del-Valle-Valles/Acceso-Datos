5b534b32c0de476646279d762a8e3af237cf716e
Alejandro Del Valle <alejandrodelvalle06@outlook.es>
2025-10-16 19:51:19 +0200

    No consigo conectar a la BBDD

---
+++ Tema_04/EmpresaADONET/Program.cs
@@ -0,0 +1,12 @@
+﻿using EmpresaADONET.Service;
+
+namespace EmpresaADONET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBService.CheckConecction();
+        }
+    }
+}
---
+++ Tema_04/EmpresaADONET/Service/DBService.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+
+namespace EmpresaADONET.Service
+{
+    internal static class DBService
+    {
+        private static readonly string _path = "HOST=localhost;Port=5432;Database=empresa;Username=admin;Password=1234;";
+
+        public static void CheckConecction()
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(_path))
+                {
+                    connection.Open();
+                    Console.WriteLine("Se pudo conectar perfectamente a la BBDD.");
+                }
+            }
+            catch (Exception ex)
+            {
+                {
+                    Console.WriteLine($"No se pudo conectar a la BBDD porqué: {ex.Message}");
+                }
+            }
+        }
+    }
+}
